5c213711eb022f3ff85cb36b46f4f94ae33b7ee9
Sk Shamim Iqbal <118110658+shamim-iq@users.noreply.github.com>
2025-01-04 01:59:42 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-aws-bucket1122"
+    bucket         = "shamim-aws-bucket"
     region         = "us-east-2"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
